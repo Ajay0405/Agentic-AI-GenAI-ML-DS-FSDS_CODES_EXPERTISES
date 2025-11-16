66ab743d5a0a60955db46a7a244108cdba47f694
Ajay0405 <ajaygangarapu@gmail.com>
2025-11-16 23:40:05 +0530

    frist commit

--- 13th Nov - Streamlit Code/Streamlit code Lab Practies/wlc.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st 
-
-st.title("Wel Come")
-st.write("Data Science World")
--- 13th Nov - Streamlit Code/Streamlit code Lab Practies/writ.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.write("Text with write")
-
-# Writing python inbuilt function range()
-st.write(range(10))
